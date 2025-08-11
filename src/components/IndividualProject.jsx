85f582b89ebe6551db2673b653bfe8355d4ae306
Ben Koenig <70115233+taqft@users.noreply.github.com>
2025-08-11 16:23:47 -0600

    update links

--- src/components/IndividualProject.jsx
+++ src/components/IndividualProject.jsx
@@ -9,11 +9,11 @@ import moonbaypreview from "../assets/moonbay-preview.png";
 export const IndividualProject = () => {
 
   const projects = [
-    { id: 1, name: "", img: blogpreview, demo: 'https://techblogmvc.up.railway.app/', code: 'https://github.com/RickyRicer/techBlogMVC'},
+    { id: 1, name: "", img: blogpreview, demo: 'https://techblogmvc.up.railway.app/', code: 'https://github.com/taqft/techBlogMVC'},
     { id: 2, name: "", img: emspreview, demo: 'https://github.com/taqft/CMSApp/blob/main/assets/images/demo.gif', code: 'https://github.com/taqft/CMSApp' },
     { id: 3, name: "", img: plannerpreview, demo: 'https://taqft.github.io/work_planner/', code: 'https://github.com/taqft/work_planner' },
-    { id: 4, name: "", img: weatherpreview, demo: 'https://atmention.github.io/weather_app/', code: 'https://github.com/atmention/weather_app' },
-    { id: 5, name: "", img: namepasspreview, demo: 'https://namepasspro.up.railway.app/', code: 'https://github.com/taqft/NamePassPro'},
+    { id: 4, name: "", img: weatherpreview, demo: 'https://taqft.github.io/weather_app/', code: 'https://github.com/taqft/weather_app' },
+    { id: 5, name: "", img: namepasspreview, demo: 'https://taqft.github.io/namepass/', code: 'https://github.com/taqft/NamePassPro'},
     { id: 6, name: "", img: moonbaypreview, demo: 'https://moonbay.up.railway.app/', code: 'https://github.com/taqft/MoonBay'}
   ];
 
